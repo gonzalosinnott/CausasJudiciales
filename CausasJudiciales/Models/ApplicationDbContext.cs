7cb16972a91f3448d92b55e098fac67dad550b45
gonzalosinnott <gonzalo.sinnott@gmail.com>
2021-08-31 11:56:32 -0300

    Modelo Asesor

---
+++ CausasJudiciales/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CausasJudiciales.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<Asesor> Asesor { get; set; }
+    }
+}
---
+++ CausasJudiciales/Models/Asesor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CausasJudiciales.Models
+{
+    public class Asesor
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public int NumeroExpediente { get; set; }
+       
+        [Display(Name = "Representado")]
+        public string Representado { get; set; }
+        
+        [Display(Name = "Caratula de la Causa")]
+        public  string Caratula { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de aceptacion de Cargo")]
+        public DateTime? AceptaCargo { get; set; }
+        
+        [Display(Name = "Actuacion")]
+        public string Actuacion { get; set; }
+
+        [Display(Name = "Regulacion")]
+        public string Regulacion { get; set; }
+
+        [Display(Name = "Observaciones")]
+        public string Observaciones { get; set; }
+
+    }
+}
